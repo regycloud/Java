1942f4af96226dd92243f6b2f480d4d0c55d9c34
regycloud <regycloud@yahoo.com>
2023-01-15 22:03:07 +0700

    fix the error

--- Language.java
+++ Language.java
@@ -1,32 +1,33 @@
 class Language {
-  protected String name;
-  protected int numSpeakers;
-  protected String regionsSpoken;
-  protected String wordOrder;
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
 
-  Language(String langName, int speakers, String regions, String wdOrder) {
-    this.name = langName;
-    this.numSpeakers = speakers;
-    this.regionsSpoken = regions;
-    this.wordOrder = wdOrder;
+    Language(String langName, int speakers, String regions, String wdOrder) {
+        this.name = langName;
+        this.numSpeakers = speakers;
+        this.regionsSpoken = regions;
+        this.wordOrder = wdOrder;
 
-  }
+    }
 
-  public void getInfo() {
-    System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".");
-    System.out.println("The language follows the order: " + this.wordOrder);
+    public void getInfo() {
+        System.out.println(
+                this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".");
+        System.out.println("The language follows the order: " + this.wordOrder);
 
-  }
+    }
 
-  public static void main(String[] args) {
-    Language bahasa = new Language("Bahasa Indonesia", 265000000, "Indonesia", "Subject-verb-object");
-  bahasa.getInfo();
-    Mayan chontal = new Mayan("Chontal", 36810);
-    chontal.getInfo();
+    public static void main(String[] args) {
+        Language bahasa = new Language("Bahasa Indonesia", 265000000, "Indonesia", "Subject-verb-object");
+        bahasa.getInfo();
+        Mayan chontal = new Mayan("Chontal", 36810);
+        chontal.getInfo();
 
-    SinoTibetan mandarin = new SinoTibetan("Mandarin Chinese", 218);
-    mandarin.getInfo();
-    SinoTibetan burmese = new SinoTibetan ("Burmese", 317);
-    burmese.getInfo();
-  }
+        SinoTibetan mandarin = new SinoTibetan("Mandarin Chinese", 218);
+        mandarin.getInfo();
+        SinoTibetan burmese = new SinoTibetan("Burmese", 317);
+        burmese.getInfo();
+    }
 }
